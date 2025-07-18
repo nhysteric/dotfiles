7dcddbe354ba11b74dd20c942b17aad76afd73cb
nhysteric <nhysteric@gmail.com>
2025-07-18 23:07:55 +0800

    add

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -x GOPATH /home/nhy/go
-set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
-source (/usr/local/bin/starship init fish --print-full-init | psub)
